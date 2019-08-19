b4edb96fe3d822f2ece015a4fe299fde393cc555
최유진 <yujinchoi@theopusone.com>
2019-08-19 11:10:38 +0900

    realmManager에 메세지 기능 추가

---
+++ oorealmmanager/src/main/java/com/opusone/leanon/oorealmmanager/model/OoRmMessage.kt
@@ -0,0 +1,20 @@
+package com.opusone.leanon.oorealmmanager.model
+
+import com.opusone.leanon.oorealmmanager.OoRealmManager
+import io.realm.RealmObject
+import io.realm.annotations.Index
+
+open class OoRmMessage : RealmObject(){
+    @Index
+    var index : Long = OoRealmManager.getMessageCount() + 1
+    var dataType: Int = -1
+    var picture : String? = ""
+    var id : String? = ""
+    var name : String? = ""
+    var message: String? = ""
+    var timestamp: Long? = -1
+
+    override fun toString(): String {
+        return "OoRmMessage(index=$index, dataType=$dataType, picture=$picture, id=$id, name=$name, message=$message, timestamp=$timestamp)"
+    }
+}
