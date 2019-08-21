ba53a5bfe20ffcfbfc70ee0be9c56d3db019f5bd
최유진 <yujinchoi@theopusone.com>
2019-08-21 14:34:06 +0900

    restManager OoRaramMessage 파라미터 수정

--- oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/params/OoNotification.kt
+++ oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/params/OoNotification.kt
@@ -1,7 +1,7 @@
 package com.opusone.leanon.oorestmanager.params
 
-class OoParamMessage(var userToken: String? = null, var to: String?, var message: String? = null, var timestamp: String? = null) {
+class OoParamMessage(var userToken: String? = null, var to: String?, var index: String?, var dataType : String?, var message: String? = null, var messageAnswer: MutableList<String?>, var timestamp: String? = null) {
     override fun toString(): String {
-        return "OoParamMessage(userToken='$userToken', to='$to', message='$message', timestamp='$timestamp')"
+        return "OoParamMessage(userToken=$userToken, to=$to, index=$index, dataType=$dataType, message=$message, messageAnswer=$messageAnswer, timestamp=$timestamp)"
     }
 }
