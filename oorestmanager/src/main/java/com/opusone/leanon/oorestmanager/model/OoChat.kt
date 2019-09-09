5997cb3e72471824483be41be63935fec9eb6ba4
최유진 <yujinchoi@theopusone.com>
2019-09-09 16:36:23 +0900

    rest groupChat response 수정

--- oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/model/OoChat.kt
+++ oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/model/OoChat.kt
@@ -16,7 +16,8 @@ class OoMessage (var user: OoCompactUser? = null,
     }
 }
 
-class OoChat (var senderId: String? = null,
+class OoChat (var user: OoCompactUser? = null,
+              var senderId: String? = null,
               var roomId: String? = null,
               var type: String? = null,
               var message: String? = null,
@@ -25,8 +26,6 @@ class OoChat (var senderId: String? = null,
               var timestamp: Long? = null): Serializable {
 
     override fun toString(): String {
-        return "OoChat ('senderId=$senderId'," +
-                " 'roomId=$roomId', 'type=$type', 'message=$message', 'answer=$answer', " +
-                "'question=$question', 'timestamp=$timestamp')"
+        return "OoChat(user=$user, senderId=$senderId, roomId=$roomId, type=$type, message=$message, answer=$answer, question=$question, timestamp=$timestamp)"
     }
 }
