d354e0474e13b1497345fa412be2bf58c170bbd4
최유진 <yujinchoi@theopusone.com>
2019-09-19 11:06:07 +0900

    오타수정

--- oorealmmanager/src/main/java/com/opusone/leanon/oorealmmanager/model/OoRmMessage.kt
+++ oorealmmanager/src/main/java/com/opusone/leanon/oorealmmanager/model/OoRmMessage.kt
@@ -12,7 +12,7 @@ open class OoRmMessage : RealmObject() {
     var dataType: Int? = -1
     var picture : String? = ""
     var id : String? = ""
-    var chatrommId : String? = ""
+    var chatroomId : String? = ""
     var name : String? = ""
     var message: String? = ""
     var messageAnswer : RealmList<String?>? = RealmList()
@@ -20,6 +20,6 @@ open class OoRmMessage : RealmObject() {
     var question: String? = ""
 
     override fun toString(): String {
-        return "OoRmMessage(index=$index, chatrommId=$chatrommId,dataType=$dataType, picture=$picture, id=$id, name=$name, message=$message, messageAnswer=$messageAnswer, timestamp=$timestamp, question=$question)"
+        return "OoRmMessage(index=$index, chatroomId=$chatroomId,dataType=$dataType, picture=$picture, id=$id, name=$name, message=$message, messageAnswer=$messageAnswer, timestamp=$timestamp, question=$question)"
     }
 }
