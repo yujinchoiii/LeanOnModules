ad29892538fd6732da7d04a19e9db4f6f3a11fb6
최유진 <yujinchoi@theopusone.com>
2019-09-26 18:09:00 +0900

    content resolver refactor(파트너정보 제거)

--- oocontentresolver/src/main/java/com/opusone/leanon/oocontentresolver/OoContentResolver.kt
+++ oocontentresolver/src/main/java/com/opusone/leanon/oocontentresolver/OoContentResolver.kt
@@ -11,15 +11,9 @@ object OoContentResolver {
 
     private val AUTHORITY = "com.opusone.leanon.oorealmprovider"
     private val USER_PATH = "user"
-    private val PARTNER_PATH = "partner"
     private val USER_OBJECT = "user_object"
-    private val PARTNER_OBJECT = "partner_object"
     private val USER_URI = Uri.parse("content://$AUTHORITY/$USER_PATH")
-    private val PARTNER_URI = Uri.parse("content://$AUTHORITY/$PARTNER_PATH")
-
-
     private val USER_PROJECTION = arrayOf(USER_OBJECT)
-    private val PARTNER_PROJECTION = arrayOf(PARTNER_OBJECT)
 
     fun getUserInfo(contentResolver: ContentResolver): User? {
         var user = User()
@@ -36,21 +30,4 @@ object OoContentResolver {
         }
         return user
     }
-
-    fun getPartnerInfo(contentResolver: ContentResolver): Partner? {
-        var partner = Partner()
-        val cursor = contentResolver.query(Uri.parse(PARTNER_URI.toString()), PARTNER_PROJECTION, null, null, null)
-        cursor?.let {
-            it.apply {
-                if (it.count <= 0) {
-                    return null
-                }
-                it.moveToFirst()
-                partner = Gson().fromJson(getString(getColumnIndex(PARTNER_PROJECTION[0])), Partner::class.java)
-            }
-            cursor.close()
-        }
-        return partner
-    }
-
 }
