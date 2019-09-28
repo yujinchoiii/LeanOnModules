c62b0e727988df06e2cb45b8f89c50672c13ae3f
choe, yeong-hyeon <cyh3813@gmail.com>
2019-09-28 19:01:08 +0900

    RestManager 복약 데이터 타입 변경

--- oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/model/OoMedication.kt
+++ oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/model/OoMedication.kt
@@ -3,17 +3,16 @@ package com.opusone.leanon.oorestmanager.model
 import java.io.Serializable
 
 class OoMedication(
-    var id: String?= null,
-    var userId: String?= null,
     var guardian: String?= null,
     var alarmId: String?= null,
     var hour: String?= null,
     var min: String?= null,
     var name: String?= null,
     var picture: String?= null,
+    var timestamp: Long = 0,
     var weekdaysInfo: MutableList<Boolean>?= null) : Serializable {
     override fun toString(): String {
-        return "OoMedication(id='$id', userId='$userId', guardian='$guardian' ,alarmId='$alarmId', " +
+        return "OoMedication(timestamp='$timestamp', guardian='$guardian' ,alarmId='$alarmId', " +
                 "guardian='$guardian', hour='$hour', min='$min', name='$name', " +
                 "picture='$picture', weekdaysInfo='$weekdaysInfo')"
     }
