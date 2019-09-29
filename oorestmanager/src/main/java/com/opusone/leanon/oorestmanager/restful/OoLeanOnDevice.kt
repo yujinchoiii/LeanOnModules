cbd63cc3eb3718d37635d84c8bfa42111597dd13
choe, yeong-hyeon <cyh3813@gmail.com>
2019-09-29 14:58:06 +0900

    - 린온 디바이스에 유저 정보 추가

--- oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/restful/OoLeanOnDevice.kt
+++ oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/restful/OoLeanOnDevice.kt
@@ -1,6 +1,7 @@
 package com.opusone.leanon.oorestmanager.restful
 
 import android.os.Build
+import com.opusone.leanon.oorestmanager.model.OoUser
 
 object OoLeanOnDevice {
     var type = OoLeanonDeviceType.UNKNOWN.type
@@ -9,8 +10,7 @@ object OoLeanOnDevice {
 
     var userId = ""
     var userToken = ""
-    var userName = ""
-    var userPicture = ""
+    var user: OoUser? = null
 
     val assessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoVG9rZW4iOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJa05VVWtGUFpFWmlabWRSYjFCNldqWnRSamxNSWl3aVpXMWhhV3dpT2lKa1pYWkFkR2hsYjNCMWMyOXVaUzVqYjIwaUxDSnVZVzFsSWpvaWIzQjFjMjl1WlNJc0luTm9ZV1J2ZHlJNkltUm1OV00yWTJVd1pEVXpZelkwT1daallURmtNVEJqT1RFMU16UmtNRGRtTnprMFlUYzVZVEJqTlRWaE1qWTBNbVpoTm1Zd05HVTBZVEpqTkRJeE4yRXlPV1l4TkRJNVpqQTFOMkkwWmpFMU5qazJORGxpWTJRME56TXlNamRrTVdZd1l6SXdOV0ZsTUdGbU9EVmxPVEF4TURVeU16QmhZMlkyTlRnM1pXTTBJaXdpWlhod2FYSmxJam9pTUNJc0ltbGhkQ0k2TVRVMk9USTVOakV3TjMwLmdtR1BlcTJFV3Y1UTJDMVRSQm5IbDRnVFhReEJGMDZ4blRFWHVTS1JONE0iLCJwYXJ0bmVySWQiOiJDVFJBT2RGYmZnUW9Qelo2bUY5TCIsImlhdCI6MTU2OTI5NjEwN30.VswXXj49no5YgCErUbmEawR7HuOwrMoSWAi2jmqCG6g"
     val os = "android"
@@ -23,13 +23,12 @@ object OoLeanOnDevice {
 
         userId = ""
         userToken = ""
-        userName = ""
-        userPicture = ""
+        user = null
     }
 
     override fun toString(): String {
         return "OoLeanOnDevice (type=$type, deviceToken =$deviceToken , nationalCode=$nationalCode, " +
-                "os=$os, model=$model, version=$version, userId=$userId, userName=$userName, userPicture=$userPicture, " +
+                "os=$os, model=$model, version=$version, userId=$userId, user=$user" +
                 "userToken=$userToken, assessToken=$assessToken)"
     }
 }
