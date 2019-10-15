6cbed3e76fa3f9f0cf73b7f047ee6a0c60479a4a
choe, yeong-hyeon <cyh3813@gmail.com>
2019-10-15 13:52:08 +0900

    1. 체중계 데이터 명칭 수정

--- oorealmmanager/src/main/java/com/opusone/leanon/oorealmmanager/model/OoRmScale.kt
+++ oorealmmanager/src/main/java/com/opusone/leanon/oorealmmanager/model/OoRmScale.kt
@@ -10,13 +10,13 @@ open class OoRmScale : RealmObject() {
     var boneMass: String? = null            //뼈무게
     var heartRate: String? = null           //심박수
     var muscleRate: String? = null          //근육량
-    var subcutaneousFat: String? = null     //피라지방
+    var subcutaneousFat: String? = null     //피하지방
     var visceralFat: String? = null         //내장지방
     var weight: String? = null              //몸부게
 
     var bodyAge: String? = null             //신체나이
+    var bodyShape: String? = null           //체형
     var protein: String? = null             //단백질
-    var bodyForm: String? = null            //체형
-    var boneMuscleMass: String? = null      //골근격량
-    var fatRemovalWeight: String? = null    //지방제거 체중
+    var mucsleMass: String? = null          //골근격량?
+    var lbm: String? = null                 //무지방 체중
 }
