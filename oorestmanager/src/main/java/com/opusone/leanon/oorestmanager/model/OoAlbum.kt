61233e26c8dd66744147978f3092e83e102a4169
choe, yeong-hyeon <cyh3813@gmail.com>
2019-10-16 11:52:12 +0900

    1. RestManager 앨범 푸시 데이터 타입 수정

--- oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/model/OoAlbum.kt
+++ oorestmanager/src/main/java/com/opusone/leanon/oorestmanager/model/OoAlbum.kt
@@ -20,8 +20,9 @@ class OoAlbumPicture (var authorId: String? = null,
 class OoPushAlbum (var user: OoCompactUser? = null,
                    var url:String? = null,
                    var albumId:String? = null,
-                   var thumb: String?= null,
+                   var thumbnail: String?= null,
                    var comment: String?=null): Serializable {
+
     override fun toString(): String {
         return "OoPushAlbum(user=$user, url=$url)"
     }
